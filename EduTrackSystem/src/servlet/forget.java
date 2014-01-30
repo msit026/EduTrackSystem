02662a41952dd19977da65f6c2d24bd0c0d0fb91
Krishna Somayajulu <Krishna Somayajulu@Krishna>
2014-01-30 12:21:48 +0530

    Added Login Page with proper validations

---
+++ EduTrackSystem/src/servlet/forget.java
@@ -0,0 +1,43 @@
+package servlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class forget
+ */
+public class forget extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public forget() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println ("<html><body>Please check your mail for further details</body></html>");
+	     // out.println("<p align =\"center\"> <a  href=\"blogger.html\" onClick=\"return targetopener(this,true)\">Back</a> </p>") ;
+		 out.println("<html><p align =\"center\"> <a  href=\"mylogin.html\" onClick=\"return targetopener(this,true)\">BACK</a> </p></html>") ;
+    	 
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request,response);
+		// TODO Auto-generated method stub
+	}
+
+}
